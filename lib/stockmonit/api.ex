dfb721b5fb8f54983742e82b6c52bd8475158477
Jozef Chraplewski <jozef@vzaar.com>
2020-02-04 22:43:57 +0100

    Refactor Stockmonit.Api.implemented

--- lib/stockmonit/api.ex
+++ lib/stockmonit/api.ex
@@ -1,8 +1,8 @@
 defmodule Stockmonit.Api do
   def monitor(stock, %{"api" => api_provider, "api_key" => api_key}) do
     case implemented(api_provider) do
-      :error -> "not implemented"
-      api -> api.fetch(stock, api_key)
+      {:error, _} -> "not implemented"
+      {:module, api} -> api.fetch(stock, api_key)
     end
   end
 
@@ -12,21 +12,15 @@ defmodule Stockmonit.Api do
   ## Examples
 
       iex> Stockmonit.Api.implemented("NotImplemented")
-      :error
+      {:error, :nofile}
 
       iex> Stockmonit.Api.implemented("Finnhub")
-      Stockmonit.Api.Finnhub
+      {:module, Stockmonit.Api.Finnhub}
   """
 
   def implemented(api_provider) do
-    compiled =
-      ("Elixir.Stockmonit.Api." <> api_provider)
-      |> String.to_atom()
-      |> Code.ensure_compiled()
-
-    case compiled do
-      {:error, _} -> :error
-      {:module, api} -> api
-    end
+    ("Elixir.Stockmonit.Api." <> api_provider)
+    |> String.to_atom()
+    |> Code.ensure_compiled()
   end
 end
