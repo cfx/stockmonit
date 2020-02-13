5cfea77dc5954067dbbd13c8b0dd8d9f4600ec54
Jozef Chraplewski <jozef@vzaar.com>
2020-02-13 21:38:27 +0100

    Add real call to Finnhub API

--- lib/stockmonit/api/finnhub.ex
+++ lib/stockmonit/api/finnhub.ex
@@ -1,5 +1,19 @@
 defmodule Stockmonit.Api.Finnhub do
   def fetch(%{"name" => name, "symbol" => symbol}, api_key) do
-    IO.puts("#{name} stock: #{symbol} key: #{api_key}")
+    url(symbol, api_key)
+    |> HTTPoison.get()
+    |> handle_response
+  end
+
+  defp url(stock_symbol, token) do
+    u = "https://finnhub.io/api/v1/quote?symbol=#{stock_symbol}&token=#{token}"
+    IO.puts(u)
+    u
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}), do: Poison.decode(body)
+
+  def handle_response({_, %{status_code: sc, body: body}}) do
+    {:error, "API error: status: #{sc} , '#{body}'"}
   end
 end
--- lib/stockmonit/stock_worker.ex
+++ lib/stockmonit/stock_worker.ex
@@ -1,5 +1,6 @@
 defmodule Stockmonit.StockWorker do
   use GenServer
+  alias Stockmonit.Api.Finnhub
 
   def start_link(stock, api_config) do
     GenServer.start_link(__MODULE__, {stock, api_config})
@@ -10,14 +11,13 @@ defmodule Stockmonit.StockWorker do
     {:ok, config}
   end
 
-  def handle_info(:fetch, {stock, api_config}) do
-    IO.inspect(stock)
-    IO.inspect(api_config)
+  def handle_info(:fetch, {stock, api_config = %{"api_key" => api_key}}) do
     #    fetch_stock()
+    IO.inspect(Finnhub.fetch(stock, api_key))
     {:noreply, {stock, api_config}}
   end
 
   defp fetch_stock() do
-    Process.send_after(self(), :fetch, 0)
+    Process.send_after(self(), :fetch, 1000)
   end
 end
