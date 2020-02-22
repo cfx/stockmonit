48ee581de9293e18f1bae0061c8b2c422fedf762
Jozef Chraplewski <jozef@vzaar.com>
2020-02-22 10:16:25 +0100

    Add Config.Server.find_stock()

--- lib/stockmonit/config/server.ex
+++ lib/stockmonit/config/server.ex
@@ -1,6 +1,9 @@
 defmodule Stockmonit.Config.Server do
   use GenServer
   alias Stockmonit.Config
+  alias Config.Stock
+
+  ### API ###
 
   def init(path) do
     case config_reader().read(path) do
@@ -20,18 +23,36 @@ defmodule Stockmonit.Config.Server do
   def get(), do: GenServer.call(__MODULE__, :get)
   def get_providers(), do: GenServer.call(__MODULE__, :get_providers)
 
-  def handle_call(:get, _from, config), do: {:reply, config, config}
+  def handle_call(:get, _, config), do: {:reply, config, config}
 
-  def handle_call(:get_providers, _from, cfg = %Config{providers: providers}) do
+  def handle_call(:get_providers, _, cfg = %Config{providers: providers}) do
     {:reply, providers, cfg}
   end
 
-  def handle_info(:init_stocks, config) do
-    %Config{stocks: stocks} = config
-    Enum.each(stocks, &Stockmonit.StocksSupervisor.add_stock(&1))
-    {:noreply, config}
+  def handle_call({:find_stock, stock_symbol}, _, cfg) do
+    {:reply, find_stock(cfg.stocks, stock_symbol), cfg}
   end
 
+  def handle_info(:init_stocks, cfg = %Config{stocks: stocks}) do
+    stocks |> Enum.each(&Stockmonit.StocksSupervisor.add_stock(&1))
+    {:noreply, cfg}
+  end
+
+  ### Impl ###
+
+  @doc """
+  Finds Stock by symbol in %Config.stocks
+  """
+
+  @spec find_stock([Stock.t()], String.t()) :: Stock.t() | nil
+  def find_stock(stocks, stock_symbol) do
+    stocks
+    |> Enum.find(fn s ->
+      s.symbol == stock_symbol
+    end)
+  end
+
+  @spec config_reader() :: Config.Reader
   defp config_reader() do
     Application.get_env(:stockmonit, :config_reader)
   end
--- test/stockmonit/config/server_test.exs
+++ test/stockmonit/config/server_test.exs
@@ -1,11 +1,25 @@
 defmodule Stockmonit.Config.ServerTest do
   use ExUnit.Case
   doctest Stockmonit.Config.Server
-  alias Stockmonit.Config
+  alias Stockmonit.Config.{Server, Stock}
+
+  setup_all do
+    [stocks: [%Stock{symbol: "FOO"}, %Stock{symbol: "BAR"}]]
+  end
 
   test "stops process when config file returns an error" do
     Process.flag(:trap_exit, true)
     expected = {:error, "Can't load config file"}
-    assert ^expected = Config.Server.start_link("/invalid/path")
+    assert ^expected = Server.start_link("/invalid/path")
+  end
+
+  describe ".find_stock()" do
+    test "returns %Stock when found", %{stocks: stocks} do
+      assert Server.find_stock(stocks, "FOO") == %Stock{symbol: "FOO"}
+    end
+
+    test "returns nil when not found", %{stocks: stocks} do
+      assert Server.find_stock(stocks, "FOO") == %Stock{symbol: "FOO"}
+    end
   end
 end
